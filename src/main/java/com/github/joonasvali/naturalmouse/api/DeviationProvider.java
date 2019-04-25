f98e8245c42b1896a040ca3aa74c2fbb2a690559
Joonas Vali <joonas.wali@gmail.com>
2019-04-25 22:30:00 +0300

    improve doc on deviationProvider and noiseProvider

--- src/main/java/com/github/joonasvali/naturalmouse/api/DeviationProvider.java
+++ src/main/java/com/github/joonasvali/naturalmouse/api/DeviationProvider.java
@@ -6,8 +6,30 @@ import com.github.joonasvali.naturalmouse.support.DoublePoint;
  * Creates arcs or deviation into mouse movement.
  *
  * DeviationProvider implementation should be immutable.
+ *
  */
 public interface DeviationProvider {
 
+  /**
+   * Gets the deviation for current trajectory.
+   *
+   * Deviation is different from the Noise because it works like a mathematical function, the resulting
+   * Point is added to single trajectory point and it will not have any effect in the next
+   * mouse movement step, making it easy to implement this as a formula based on the input parameters.
+   * e.g the something like 'deviation = totalDistanceInPixels / completionFraction', resulting in smooth movement.
+   * (Don't actually use this formula)
+   *
+   * While 'Noise' from the {@link (com.github.joonasvali.naturalmouse.api.NoiseProvider)} is generating an offset
+   * from the original trajectory and is appended to the original trajectory, having a possibility of accumulating
+   * over time.
+   *
+   * As deviation is a 'function' it should not include Random behaviour, thus Random is not included as a parameter.
+   *
+   * @param totalDistanceInPixels the total pixels between target and mouse initial position
+   * @param completionFraction the completed fraction of mouse movement total distance, value from 0...1 (0;1]
+   * @return a point which describes how much the mouse is going to deviate from the straight trajectory between
+   * target and initial position. This is not the final deviation of the mouse as MouseMotion will randomly decide
+   * to either amplify or decrease it over the whole mouse movement, making the resulting arc stand out more or less.
+   */
   DoublePoint getDeviation(double totalDistanceInPixels, double completionFraction);
 }
--- src/main/java/com/github/joonasvali/naturalmouse/api/NoiseProvider.java
+++ src/main/java/com/github/joonasvali/naturalmouse/api/NoiseProvider.java
@@ -10,5 +10,21 @@ import java.util.Random;
  * NoiseProvider implementation should be immutable.
  */
 public interface NoiseProvider {
+  /**
+   * 'Noise' is generating an offset from straight movement, and is accumulating, so on average it should create
+   * an equal chance of either positive or negative movement, otherwise the mouse movement will always be slightly
+   * offset to single direction.
+   *
+   * Deviation from {@link (com.github.joonasvali.naturalmouse.api.DeviationProvider)} is different from the Noise
+   * because it works like a mathematical function, the resulting Point is added to single trajectory point and it
+   * will not have any effect in the next mouse movement step, making it easy to implement this as a formula
+   * based on the input parameters.
+   *
+   * @param random use this to generate randomness in the offset
+   * @param xStepSize the step size that is taken horizontally
+   * @param yStepSize the step size that is taken vertically
+   * @return a point which describes how much the mouse offset is increased or decreased, depending on previous state.
+   *
+   */
   DoublePoint getNoise(Random random, double xStepSize, double yStepSize);
 }
