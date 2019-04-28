9704cfa108f083dd26df0b4c223c3b6cd40b5f69
Joonas Vali <joonas.wali@gmail.com>
2019-04-28 11:39:45 +0300

    prep 2.0.1 and polish SystemDiagnosis class a bit

--- src/main/java/com/github/joonasvali/naturalmouse/tools/SystemDiagnosis.java
+++ src/main/java/com/github/joonasvali/naturalmouse/tools/SystemDiagnosis.java
@@ -9,11 +9,14 @@ import java.awt.*;
 
 public class SystemDiagnosis {
 
-  public static void main(String[] args) {
-    new SystemDiagnosis().validateMouseMovement();
-  }
-
-  public void validateMouseMovement() {
+  /**
+   * Runs a diagnosis with default configuration, by setting mouse all over your screen and expecting to receive
+   * correct coordinates back.
+   * If java.awt.Robot cannot be constructed, then new RuntimeException is thrown.
+   * If no issues are found, then this method completes without throwing an error, otherwise IllegalStateException is
+   * thrown.
+   */
+  public static void validateMouseMovement() {
     try {
       Robot robot = new Robot();
       validateMouseMovement(new DefaultSystemCalls(robot), new DefaultMouseInfoAccessor());
@@ -22,7 +25,14 @@ public class SystemDiagnosis {
     }
   }
 
-  public void validateMouseMovement(SystemCalls system, MouseInfoAccessor accessor) {
+  /**
+   * Runs a diagnosis, by setting mouse all over your screen and expecting to receive correct coordinates back.
+   * If no issues are found, then this method completes without throwing an error, otherwise IllegalStateException is
+   * thrown.
+   * @param system a SystemCalls class which is used for setting the mouse position
+   * @param accessor a MouseInfoAccessor which is used for querying mouse position
+   */
+  public static void validateMouseMovement(SystemCalls system, MouseInfoAccessor accessor) {
     Dimension dimension = system.getScreenSize();
     for (int y = 0; y < dimension.height; y += 50) {
       for (int x = 0; x < dimension.width; x += 50) {
@@ -36,7 +46,7 @@ public class SystemDiagnosis {
 
         Point p = accessor.getMousePosition();
         if (x != p.x || y != p.y) {
-          throw new IllegalArgumentException(
+          throw new IllegalStateException(
               "Tried to move mouse to (" + x + ", " + y + "). Actually moved to (" + p.x + ", " + p.y + ")" +
               "This means NaturalMouseMotion is not able to work optimally on this system as the cursor move " +
               "calls may miss the target pixels on the screen."
