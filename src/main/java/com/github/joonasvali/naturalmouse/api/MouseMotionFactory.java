660b1169283fee64e600fd8fca0d0022b5a42ffd
JoonasVali <joonas.wali@gmail.com>
2021-09-16 18:23:09 +0300

    reformat tabs to spaces

--- src/main/java/com/github/joonasvali/naturalmouse/api/MouseMotionFactory.java
+++ src/main/java/com/github/joonasvali/naturalmouse/api/MouseMotionFactory.java
@@ -1,204 +1,204 @@
 package com.github.joonasvali.naturalmouse.api;
 
-import java.util.Random;
-
 import com.github.joonasvali.naturalmouse.support.DefaultMouseMotionNature;
 import com.github.joonasvali.naturalmouse.support.MouseMotionNature;
+
+import java.util.Random;
 
 /**
  * This class should be used for creating new MouseMotion-s The default instance
  * is available via getDefault(), but can create new instance via constructor.
  */
 public class MouseMotionFactory {
-	private static MouseMotionFactory defaultFactory;
-	private MouseMotionNature nature;
-	private Random random = new Random();
+  private static MouseMotionFactory defaultFactory;
+  private MouseMotionNature nature;
+  private Random random = new Random();
 
-	public MouseMotionFactory(final MouseMotionNature nature) {
-		this.nature = nature;
-	}
+  public MouseMotionFactory(final MouseMotionNature nature) {
+    this.nature = nature;
+  }
 
-	public MouseMotionFactory() {
-		this(new DefaultMouseMotionNature());
-	}
+  public MouseMotionFactory() {
+    this(new DefaultMouseMotionNature());
+  }
 
-	/**
-	 * Builds the MouseMotion, which can be executed instantly or saved for later.
-	 *
-	 * @param xDest the end position x-coordinate for the mouse
-	 * @param yDest the end position y-coordinate for the mouse
-	 * @return the MouseMotion which can be executed instantly or saved for later.
-	 *         (Mouse will be moved from its current position, not from the position
-	 *         where mouse was during building.)
-	 */
-	public MouseMotion build(final int xDest, final int yDest) {
-		return new MouseMotion(nature, random, xDest, yDest);
-	}
+  /**
+   * Builds the MouseMotion, which can be executed instantly or saved for later.
+   *
+   * @param xDest the end position x-coordinate for the mouse
+   * @param yDest the end position y-coordinate for the mouse
+   * @return the MouseMotion which can be executed instantly or saved for later.
+   * (Mouse will be moved from its current position, not from the position
+   * where mouse was during building.)
+   */
+  public MouseMotion build(final int xDest, final int yDest) {
+    return new MouseMotion(nature, random, xDest, yDest);
+  }
 
-	/**
-	 * Start moving the mouse to specified location. Blocks until done.
-	 *
-	 * @param xDest the end position x-coordinate for the mouse
-	 * @param yDest the end position y-coordinate for the mouse
-	 * @throws InterruptedException if something interrupts the thread.
-	 */
-	public void move(final int xDest, final int yDest) throws InterruptedException {
-		build(xDest, yDest).move();
-	}
+  /**
+   * Start moving the mouse to specified location. Blocks until done.
+   *
+   * @param xDest the end position x-coordinate for the mouse
+   * @param yDest the end position y-coordinate for the mouse
+   * @throws InterruptedException if something interrupts the thread.
+   */
+  public void move(final int xDest, final int yDest) throws InterruptedException {
+    build(xDest, yDest).move();
+  }
 
-	/**
-	 * Get the default factory implementation.
-	 *
-	 * @return the factory
-	 */
-	public static MouseMotionFactory getDefault() {
-		if (defaultFactory == null) {
-			defaultFactory = new MouseMotionFactory();
-		}
-		return defaultFactory;
-	}
+  /**
+   * Get the default factory implementation.
+   *
+   * @return the factory
+   */
+  public static MouseMotionFactory getDefault() {
+    if (defaultFactory == null) {
+      defaultFactory = new MouseMotionFactory();
+    }
+    return defaultFactory;
+  }
 
-	/**
-	 * see {@link MouseMotionNature#getSystemCalls()}
-	 *
-	 * @return the systemcalls
-	 */
-	public SystemCalls getSystemCalls() {
-		return nature.getSystemCalls();
-	}
+  /**
+   * see {@link MouseMotionNature#getSystemCalls()}
+   *
+   * @return the systemcalls
+   */
+  public SystemCalls getSystemCalls() {
+    return nature.getSystemCalls();
+  }
 
-	/**
-	 * see {@link MouseMotionNature#setSystemCalls(SystemCalls)}
-	 *
-	 * @param systemCalls the systemcalls
-	 */
-	public void setSystemCalls(final SystemCalls systemCalls) {
-		nature.setSystemCalls(systemCalls);
-	}
+  /**
+   * see {@link MouseMotionNature#setSystemCalls(SystemCalls)}
+   *
+   * @param systemCalls the systemcalls
+   */
+  public void setSystemCalls(final SystemCalls systemCalls) {
+    nature.setSystemCalls(systemCalls);
+  }
 
-	/**
-	 * see {@link MouseMotionNature#getDeviationProvider()}
-	 *
-	 * @return the deviation provider
-	 */
-	public DeviationProvider getDeviationProvider() {
-		return nature.getDeviationProvider();
-	}
+  /**
+   * see {@link MouseMotionNature#getDeviationProvider()}
+   *
+   * @return the deviation provider
+   */
+  public DeviationProvider getDeviationProvider() {
+    return nature.getDeviationProvider();
+  }
 
-	/**
-	 * see {@link MouseMotionNature#setDeviationProvider(DeviationProvider)}
-	 *
-	 * @param deviationProvider the deviation provider
-	 */
-	public void setDeviationProvider(final DeviationProvider deviationProvider) {
-		nature.setDeviationProvider(deviationProvider);
-	}
+  /**
+   * see {@link MouseMotionNature#setDeviationProvider(DeviationProvider)}
+   *
+   * @param deviationProvider the deviation provider
+   */
+  public void setDeviationProvider(final DeviationProvider deviationProvider) {
+    nature.setDeviationProvider(deviationProvider);
+  }
 
-	/**
-	 * see {@link MouseMotionNature#getNoiseProvider()}
-	 *
-	 * @return the noise provider
-	 */
-	public NoiseProvider getNoiseProvider() {
-		return nature.getNoiseProvider();
-	}
+  /**
+   * see {@link MouseMotionNature#getNoiseProvider()}
+   *
+   * @return the noise provider
+   */
+  public NoiseProvider getNoiseProvider() {
+    return nature.getNoiseProvider();
+  }
 
-	/**
-	 * see {@link MouseMotionNature#setNoiseProvider(NoiseProvider)}}
-	 *
-	 * @param noiseProvider the noise provider
-	 */
-	public void setNoiseProvider(final NoiseProvider noiseProvider) {
-		nature.setNoiseProvider(noiseProvider);
-	}
+  /**
+   * see {@link MouseMotionNature#setNoiseProvider(NoiseProvider)}}
+   *
+   * @param noiseProvider the noise provider
+   */
+  public void setNoiseProvider(final NoiseProvider noiseProvider) {
+    nature.setNoiseProvider(noiseProvider);
+  }
 
-	/**
-	 * Get the random used whenever randomized behavior is needed in MouseMotion
-	 *
-	 * @return the random
-	 */
-	public Random getRandom() {
-		return random;
-	}
+  /**
+   * Get the random used whenever randomized behavior is needed in MouseMotion
+   *
+   * @return the random
+   */
+  public Random getRandom() {
+    return random;
+  }
 
-	/**
-	 * Set the random used whenever randomized behavior is needed in MouseMotion
-	 *
-	 * @param random the random
-	 */
-	public void setRandom(final Random random) {
-		this.random = random;
-	}
+  /**
+   * Set the random used whenever randomized behavior is needed in MouseMotion
+   *
+   * @param random the random
+   */
+  public void setRandom(final Random random) {
+    this.random = random;
+  }
 
-	/**
-	 * see {@link MouseMotionNature#getMouseInfo()}
-	 *
-	 * @return the mouseInfo
-	 */
-	public MouseInfoAccessor getMouseInfo() {
-		return nature.getMouseInfo();
-	}
+  /**
+   * see {@link MouseMotionNature#getMouseInfo()}
+   *
+   * @return the mouseInfo
+   */
+  public MouseInfoAccessor getMouseInfo() {
+    return nature.getMouseInfo();
+  }
 
-	/**
-	 * see {@link MouseMotionNature#setMouseInfo(MouseInfoAccessor)}
-	 *
-	 * @param mouseInfo the mouseInfo
-	 */
-	public void setMouseInfo(final MouseInfoAccessor mouseInfo) {
-		nature.setMouseInfo(mouseInfo);
-	}
+  /**
+   * see {@link MouseMotionNature#setMouseInfo(MouseInfoAccessor)}
+   *
+   * @param mouseInfo the mouseInfo
+   */
+  public void setMouseInfo(final MouseInfoAccessor mouseInfo) {
+    nature.setMouseInfo(mouseInfo);
+  }
 
-	/**
-	 * see {@link MouseMotionNature#getSpeedManager()}
-	 *
-	 * @return the manager
-	 */
-	public SpeedManager getSpeedManager() {
-		return nature.getSpeedManager();
-	}
+  /**
+   * see {@link MouseMotionNature#getSpeedManager()}
+   *
+   * @return the manager
+   */
+  public SpeedManager getSpeedManager() {
+    return nature.getSpeedManager();
+  }
 
-	/**
-	 * see {@link MouseMotionNature#setSpeedManager(SpeedManager)}
-	 *
-	 * @param speedManager the manager
-	 */
-	public void setSpeedManager(final SpeedManager speedManager) {
-		nature.setSpeedManager(speedManager);
-	}
+  /**
+   * see {@link MouseMotionNature#setSpeedManager(SpeedManager)}
+   *
+   * @param speedManager the manager
+   */
+  public void setSpeedManager(final SpeedManager speedManager) {
+    nature.setSpeedManager(speedManager);
+  }
 
-	/**
-	 * The Nature of mousemotion covers all aspects how the mouse is moved.
-	 *
-	 * @return the nature
-	 */
-	public MouseMotionNature getNature() {
-		return nature;
-	}
+  /**
+   * The Nature of mousemotion covers all aspects how the mouse is moved.
+   *
+   * @return the nature
+   */
+  public MouseMotionNature getNature() {
+    return nature;
+  }
 
-	/**
-	 * The Nature of mousemotion covers all aspects how the mouse is moved.
-	 *
-	 * @param nature the new nature
-	 */
-	public void setNature(final MouseMotionNature nature) {
-		this.nature = nature;
-	}
+  /**
+   * The Nature of mousemotion covers all aspects how the mouse is moved.
+   *
+   * @param nature the new nature
+   */
+  public void setNature(final MouseMotionNature nature) {
+    this.nature = nature;
+  }
 
-	/**
-	 * see {@link MouseMotionNature#setOvershootManager(OvershootManager)}
-	 *
-	 * @param manager the manager
-	 */
-	public void setOvershootManager(final OvershootManager manager) {
-		nature.setOvershootManager(manager);
-	}
+  /**
+   * see {@link MouseMotionNature#setOvershootManager(OvershootManager)}
+   *
+   * @param manager the manager
+   */
+  public void setOvershootManager(final OvershootManager manager) {
+    nature.setOvershootManager(manager);
+  }
 
-	/**
-	 * see {@link MouseMotionNature#getOvershootManager()}
-	 *
-	 * @return the manager
-	 */
-	public OvershootManager getOvershootManager() {
-		return nature.getOvershootManager();
-	}
+  /**
+   * see {@link MouseMotionNature#getOvershootManager()}
+   *
+   * @return the manager
+   */
+  public OvershootManager getOvershootManager() {
+    return nature.getOvershootManager();
+  }
 }
--- src/main/java/com/github/joonasvali/naturalmouse/support/DefaultMouseMotionNature.java
+++ src/main/java/com/github/joonasvali/naturalmouse/support/DefaultMouseMotionNature.java
@@ -1,46 +1,45 @@
 package com.github.joonasvali.naturalmouse.support;
 
+import com.github.joonasvali.naturalmouse.api.SystemCalls;
+
+import java.awt.*;
+import java.util.Random;
+
 import static com.github.joonasvali.naturalmouse.support.DefaultNoiseProvider.DEFAULT_NOISINESS_DIVIDER;
 import static com.github.joonasvali.naturalmouse.support.SinusoidalDeviationProvider.DEFAULT_SLOPE_DIVIDER;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.util.Random;
-
-import com.github.joonasvali.naturalmouse.api.SystemCalls;
-
 public class DefaultMouseMotionNature extends MouseMotionNature {
 
-	public static final int TIME_TO_STEPS_DIVIDER = 8;
-	public static final int MIN_STEPS = 10;
-	public static final int EFFECT_FADE_STEPS = 15;
-	public static final int REACTION_TIME_BASE_MS = 20;
-	public static final int REACTION_TIME_VARIATION_MS = 120;
+  public static final int TIME_TO_STEPS_DIVIDER = 8;
+  public static final int MIN_STEPS = 10;
+  public static final int EFFECT_FADE_STEPS = 15;
+  public static final int REACTION_TIME_BASE_MS = 20;
+  public static final int REACTION_TIME_VARIATION_MS = 120;
 
-	public DefaultMouseMotionNature(final SystemCalls systemCalls) {
-		setSystemCalls(systemCalls);
-		setDeviationProvider(new SinusoidalDeviationProvider(DEFAULT_SLOPE_DIVIDER));
-		setNoiseProvider(new DefaultNoiseProvider(DEFAULT_NOISINESS_DIVIDER));
-		setSpeedManager(new DefaultSpeedManager());
-		setOvershootManager(new DefaultOvershootManager(new Random()));
-		setEffectFadeSteps(EFFECT_FADE_STEPS);
-		setMinSteps(MIN_STEPS);
-		setMouseInfo(new DefaultMouseInfoAccessor());
-		setReactionTimeBaseMs(REACTION_TIME_BASE_MS);
-		setReactionTimeVariationMs(REACTION_TIME_VARIATION_MS);
-		setTimeToStepsDivider(TIME_TO_STEPS_DIVIDER);
-	}
+  public DefaultMouseMotionNature(final SystemCalls systemCalls) {
+    setSystemCalls(systemCalls);
+    setDeviationProvider(new SinusoidalDeviationProvider(DEFAULT_SLOPE_DIVIDER));
+    setNoiseProvider(new DefaultNoiseProvider(DEFAULT_NOISINESS_DIVIDER));
+    setSpeedManager(new DefaultSpeedManager());
+    setOvershootManager(new DefaultOvershootManager(new Random()));
+    setEffectFadeSteps(EFFECT_FADE_STEPS);
+    setMinSteps(MIN_STEPS);
+    setMouseInfo(new DefaultMouseInfoAccessor());
+    setReactionTimeBaseMs(REACTION_TIME_BASE_MS);
+    setReactionTimeVariationMs(REACTION_TIME_VARIATION_MS);
+    setTimeToStepsDivider(TIME_TO_STEPS_DIVIDER);
+  }
 
-	public DefaultMouseMotionNature() {
-		this(new DefaultSystemCalls(getRobot()));
+  public DefaultMouseMotionNature() {
+    this(new DefaultSystemCalls(getRobot()));
 
-	}
+  }
 
-	private static Robot getRobot() {
-		try {
-			return new Robot();
-		} catch (final AWTException e) {
-			throw new RuntimeException(e);
-		}
-	}
+  private static Robot getRobot() {
+    try {
+      return new Robot();
+    } catch (final AWTException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
